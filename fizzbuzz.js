573322027d163fe0e6783a0ac87e68a653058a3c
Charles Dunn <crdunn210@gmail.com>
2025-06-04 16:36:03 -0400

    Update fizzbuzz.js

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -2,16 +2,7 @@ function fizzbuzz(iterations) {
     let output = 0;
 
     for (let i = 1; i <= iterations; i++) {
-        if (i % 15 === 0) {
-            console.log("Fizzbuzz");
-        } else if (i % 3 === 0) {
-            console.log("Fizz")
-        } else if (i % 5 == 0) {
-            console.log("Buzz")
-        } else {
-            console.log(i);
-            output += i;
-        }
+        // Add Your Code Here
     }
 
     return output;
